0047c4bcccdd520145f40780dd3dd742d2b4459c
Gage Sturgeon <gsjets33@gmail.com>
2023-03-08 00:15:00 -0800

    Add functions for square creation

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,19 +1,38 @@
 let slider = document.querySelector('.slider');
 let valueDisplay = document.querySelector('.value');
+let drawingContainer = document.querySelector('.drawing-container');
 
+
+function createSquares(multiplyer) {
+    let rowWidth = drawingContainer.clientWidth / multiplyer;
+    
+    for (let i = 0; i < multiplyer; i++) {
+        let row = document.createElement('div');
+        row.setAttribute('style', `display: flex; flex-direction: column; height: 100%; width: ${rowWidth}px`);
+        drawingContainer.appendChild(row);
+        let squareHeight = row.clientHeight / multiplyer;
+
+        for (let i = 0; i < multiplyer; i++) {
+            let square = document.createElement('div');
+            square.setAttribute('style', `width: 100%; height: ${squareHeight}px; border: 1px; border-style: solid`);
+            row.appendChild(square);
+        }
+
+    }
+}
+
+function removeSquares(parent) {
+    while(drawingContainer.firstChild) {
+        drawingContainer.removeChild(drawingContainer.lastChild);
+    }
+}
 
 valueDisplay.textContent = `${slider.value} x ${slider.value}`;
+createSquares(slider.value);
 
 
-
-
-// slider.addEventListener('input', updateArray(), false);
-
-slider.addEventListener('input', () => 
+slider.addEventListener('input', () => {
 valueDisplay.textContent = `${slider.value} x ${slider.value}`;
-);
-
-// function updateArray() {
-//     valueDisplay.textContent = `${slider.value} x ${slider.value}`;
-
-// }
+removeSquares(drawingContainer);
+createSquares(slider.value);
+});
